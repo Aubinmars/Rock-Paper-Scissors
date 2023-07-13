b7579e9a005f05cdf19872ec34d5b45980280da6
Aubinmars <aubinmars13@gmail.com>
2023-07-13 10:19:20 +0200

    modified files

--- script.js
+++ script.js
@@ -1,78 +1,70 @@
-// Game variables
-let compChoice = {Value: ""};
-let playerChoice;
-let compChoiceInt = 0;
-let playerChoiceInt = 0;
-const buttons = document.querySelectorAll('.btn');
-
-let playerScore = 0;
-let compScore = 0;
-
-// Button elements
-const rockBtn = document.getElementById('rock');
-const paperBtn = document.getElementById('paper');
-const scissorsBtn = document.getElementById('scissors');
-
-// Score elements
+const rockButton = document.getElementById('rock');
+const paperButton = document.getElementById('paper');
+const scissorsButton = document.getElementById('scissors');
 const playerScoreElement = document.getElementById('player-score');
 const compScoreElement = document.getElementById('comp-score');
+const output = document.getElementById('output');
+let playerScore = 0;
+let computerScore = 0;
 
-// Output element
-const outputElement = document.getElementById('output');
-
-// Function to generate a random computer choice
 function getComputerChoice() {
-  const choices = ['rock', 'paper', 'scissors'];
-  const randomIndex = Math.floor(Math.random() * choices.length);
-  return choices[randomIndex];
+  let choiceNum = Math.floor(Math.random() * 3);
+  switch (choiceNum) {
+    case 0:
+      return 'rock';
+    case 1:
+      return 'paper';
+    case 2:
+      return 'scissors';
+    default:
+      return '';
+  }
 }
 
-// Function to handle game logic
-function playGame(playerChoice) {
-  const computerChoice = getComputerChoice();
+function playRound(playerChoiceInt, computerChoice) {
+  let winArray = [
+    [0, 2, 1],
+    [1, 0, 2],
+    [2, 1, 0]
+  ];
+  let result = winArray[playerChoiceInt][computerChoice];
 
-  switch (playerChoice + computerChoice) {
-    case 'rockscissors':
-    case 'paperrock':
-    case 'scissorspaper':
-      playerScore++;
-      outputElement.textContent = `You win! ${playerChoice} beats ${computerChoice}.`;
-      break;
-    case 'rockpaper':
-    case 'paperscissors':
-    case 'scissorsrock':
-      compScore++;
-      outputElement.textContent = `You lose! ${computerChoice} beats ${playerChoice}.`;
-      break;
-    case 'rockrock':
-    case 'paperpaper':
-    case 'scissorsscissors':
-      outputElement.textContent = "It's a tie!";
-      break;
+  if (result === 0) {
+    output.textContent = `It's a tie! You chose ${playerChoiceInt} and the computer chose ${computerChoice}`;
+  } else if (result === 1) {
+    output.textContent = `You won! You chose ${playerChoiceInt} and the computer chose ${computerChoice}`;
+    playerScore++;
+  } else if (result === 2) {
+    output.textContent = `You lost! You chose ${playerChoiceInt} and the computer chose ${computerChoice}`;
+    computerScore++;
   }
 
-  // Update scores on the page
   playerScoreElement.textContent = `Player Score: ${playerScore}`;
-  compScoreElement.textContent = `Computer Score: ${compScore}`;
+  compScoreElement.textContent = `Computer Score: ${computerScore}`;
 
-  // Check if a player has won
   if (playerScore === 5) {
-    outputElement.textContent = "Congratulations! You've won the game!";
+    output.textContent = 'Congratulations! You won the game!';
     disableButtons();
-  } else if (compScore === 5) {
-    outputElement.textContent = "Oh no! The computer has won the game!";
+  } else if (computerScore === 5) {
+    output.textContent = 'You lost the game. Better luck next time!';
     disableButtons();
   }
 }
 
-// Function to disable game buttons
 function disableButtons() {
-  rockBtn.disabled = true;
-  paperBtn.disabled = true;
-  scissorsBtn.disabled = true;
+  rockButton.disabled = true;
+  paperButton.disabled = true;
+  scissorsButton.disabled = true;
 }
 
-// Event listeners for the game buttons
-rockBtn.addEventListener('click', () => playGame('rock'));
-paperBtn.addEventListener('click', () => playGame('paper'));
-scissorsBtn.addEventListener('click', () => playGame('scissors'));
+rockButton.addEventListener('click', function() {
+  playRound(0, getComputerChoice());
+});
+
+paperButton.addEventListener('click', function() {
+  playRound(1, getComputerChoice());
+});
+
+scissorsButton.addEventListener('click', function() {
+  playRound(2, getComputerChoice());
+});
